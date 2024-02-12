fd878a2feaed5beecf38b0cbae37f5dc2096cf59
velox0 <veloxzerror@gmail.com>
2024-02-12 19:57:23 +0530

    update: - colourizer.c   - added int active_chunk   - added TODOs

--- src/colourizer.c
+++ src/colourizer.c
@@ -2,11 +2,25 @@
 #include <stdio.h>
 
 void colourize(const char *str, chunk begin, chunk *chunks, int chunk_count) {
+  /*
+    index of currently active chunk
+    chunk on active index will be ignored
+    chunks of same colour should also be ignored
+  */
+  int active_chunk = -1;
+
   if (begin.colourtype == 4) {
     start4(begin.colour.colour4, NOBG);
   }
 
-  printf("%s", str);
+  for (int i = 0; str[i]; i++) {
+    /*
+      match chunks
+      implement kmp (add kmp table to chunk structure)
+      activate different chunk
+    */
+    printf("%c", str[i]);
+  }
 
   // reset colour
   printf("\033[0m");
