8afd16aceb7097cd1c0f8048bbc3c130a78ea194
velox0 <veloxzerror@gmail.com>
2024-02-16 05:12:36 +0530

    update: colourize: store matching string indicies

--- src/colourizer.c
+++ src/colourizer.c
@@ -1,6 +1,7 @@
 #include "colourizer.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void chunk_init(chunk *chunk) {
   for (chunk->len = 0; chunk->match[chunk->len]; chunk->len++)
@@ -48,20 +49,51 @@ void colourize(const char *str, chunk begin, chunk *chunks, int chunk_count) {
     chunk on active index will be ignored
     chunks of same colour should also be ignored
   */
-  int active_chunk = -1;
+
+  int active_chunk = -1, // index of chunk whose colour is currently enabled
+      matches = 0;       // number of matches in string
+  int *indicies,         // index of matching string
+      *matching_chunk;   // corresponding chunk
+
+  indicies = (int *)malloc(sizeof(int) * 10);
+  matching_chunk = (int *)malloc(sizeof(int) * 10); // assuming 10 matches
+                                                    // will increase if required
+
+  for (int j = 0, start = 0, f = 0; j < chunk_count;) {
+    int ind = match(chunks[j], str, start, f);
+
+    if (ind == -1) {
+      start = 0;
+      f = 0;
+      j++;
+      continue;
+    }
+
+    // Increase size to accumulate more matches
+    if (matches > 9 && matches % 10 == 0) {
+      indicies = (int *)realloc(indicies, sizeof(int) * (matches + 10));
+      matching_chunk =
+          (int *)realloc(matching_chunk, sizeof(int) * (matches + 10));
+    }
+
+    indicies[matches] = ind;
+    matching_chunk[matches] = j;
+
+    matches++;
+    start = ind;
+    f = 1;
+  }
 
   if (begin.colourtype == 4) {
     start4(begin.colour.colour4, NOBG);
   }
 
   for (int i = 0; str[i]; i++) {
-    /*
-      match chunks
-      implement kmp (add kmp table to chunk structure)
-      activate different chunk
-    */
     printf("%c", str[i]);
   }
+
+  free(indicies);
+  free(matching_chunk);
 
   // reset colour
   printf("\033[0m");
