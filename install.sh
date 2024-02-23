1f7a1f12d631416f8c03e75f7cf340562fb5f328
velox0 <veloxzerror@gmail.com>
2024-02-24 04:45:01 +0530

    add: manpage update: install script

--- install.sh
+++ install.sh
@@ -3,4 +3,15 @@ if [ -f /usr/lib/libclr.so ]; then
 else
   echo libclr not found
   echo install from https://www.github.com/velox0/libclr
+  exit 1
 fi
+
+gzip -fk ./clr.6
+
+manpath="/usr/share/man/man6/"
+if [ ! -d "$manpath" ]; then
+  # Create the path
+  sudo mkdir -p "$manpath"
+fi
+
+sudo mv ./clr.6.* $manpath
